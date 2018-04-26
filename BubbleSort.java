d75fdb6d31f74149a34cbea36f025384eb1c93b9
joyske <38735342+joyske@users.noreply.github.com>
2018-04-26 12:03:19 +0200

    ICH HAB ALLES GEMACGR

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,4 +1,31 @@
 public class BubbleSort
 {
-	
+    public static void Bubble(int[]arr)
+    {
+        int n=arr.length;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=n-1;j>=i;j--)
+            {
+                if(arr[j-1]>arr[j])
+                {
+                    int tmp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=tmp;
+                }
+            }
+        }
+        show(arr);
+    }
+    
+    public static void show(int[]arr)
+    {
+        for(int a : arr)
+        {
+            System.out.print(a+" ");
+        }
+    }
+    
+
+
 }
