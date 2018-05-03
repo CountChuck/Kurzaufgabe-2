a029eda6ac67754f68ca9c9ccb6abb2bcd7a253a
lettiChop <38751506+lettiChop@users.noreply.github.com>
2018-05-03 10:28:58 +0200

    Kleine Änderungen hier und da...

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,105 +1,39 @@
-public class BubbleSort
-{
+public class BubbleSort {
+    
     public static void Bubble(int[] arr)
     {
         int n=arr.length;
         for(int i=1;i<n;i++)
-	//Feld wird von vorne durchlaufen
+    //Feld wird von vorne durchlaufen
         {
             for(int j=n-1;j>=i;j--)
-	//Feld wird von hinten durchlaufen
+    //Feld wird von hinten durchlaufen
             {
                 if(arr[j-1]>arr[j])
-	//überprüfen ob variable vor arr[j] größer ist
+    //überprüfen ob variable vor arr[j] größer ist
                 {
                     int tmp=arr[j];
                     arr[j]=arr[j-1];
-	//Werte werden getauscht
+    //Werte werden getauscht
                     arr[j-1]=tmp;
                 }
             }
         }
     }
-
-    public static void measure(int[] array)
-	{
-		long tStart, tEnd, lmsecs;
-		//Start der Messung der Laufzeit
-		tStart = System.currentTimeMillis();
-		Bubble(array);
-		//Ende der Messung
-		tEnd = System.currentTimeMillis();
-		//Umrechnung der Millisekunden in Sekunden und anschließende Ausgabe
-		lmsecs = (tEnd - tStart)/1000;
-		float msecs = lmsecs;
-		System.out.println("Laufzeit: " + msecs);
-    }
-	
-	public static float runtime(int[] array)
-	{
-		long tStart, tEnd, lmsecsrt;
-		//Start der Messung der Laufzeit
-		tStart = System.currentTimeMillis();
-		Bubble(array);
-		//Ende der Messung
-		tEnd = System.currentTimeMillis();
-		lmsecsrt = (tEnd - tStart)/1000;
-		float msecsrt = lmsecsrt;
-		return msecsrt;
-	}
-     
-    public static void main(String[] args)
+    
+    public static float runtime(int[] array)
     {
-		try
-		{
-			if(args.length == 1)
-			{
-				int a = Integer.parseInt(args[0]);
-				if(a>0)
-				{
-					int[]arr=makeArray(a);
-					measure(arr);
-				}
-				else
-				{
-					System.out.println("Nur positive Zahlen!");   
-				}	 
-			}
-			else
-			{
-				System.out.println("Genau eine Eingabe bitte!");
-			}
-		}
-		catch(NumberFormatException e)
-		{
-			float b = Float.parseFloat(args[0]);
-			int n = 1000;
-			int[] arr2 = makeArray(n);
-			while(b > runtime(arr2))
-			{
-				n = 2*n;
-				arr2 = makeArray(n);
-			}
-			int tmpN = n/2;
-			while(n >= tmpN)
-			{
-				int middle = (n+tmpN)/2;
-				float rtMid = runtime(makeArray(middle));
-				if(rtMid+0.1 <= b || rtMid-0.1 >= b)
-				{
-					System.out.println(runtime(makeArray(middle)));
-				}
-				if(middle < b)
-				{
-					tmpN = middle+1;
-				}
-				if(middle > b)
-				{
-					n = middle-1;
-				}
-			}
-		}
+        long tStart, tEnd, lmsecsrt;
+        //Start der Messung der Laufzeit
+        tStart = System.currentTimeMillis();
+        Bubble(array);
+        //Ende der Messung
+        tEnd = System.currentTimeMillis();
+        lmsecsrt = (tEnd - tStart);
+        float fsecsrt = (float)lmsecsrt/(float)1000;    
+        return fsecsrt;
     }
+    
     
     public static int[] makeArray(int a)
     {
@@ -113,4 +47,60 @@ public class BubbleSort
         }
         return arr;
     }
+     
+    public static void main(String[] args)
+    {
+        int[] testArray = makeArray(5000);
+		System.out.prinln("Bei einer Feldgröße von 500 ist die Laufzeit:"); 
+        System.out.printf("%.4f", runtime(testArray)); 
+        
+        try
+        {
+            if(args.length == 1)
+            {
+                float t = Float.parseFloat(args[0]);
+                if(t<0.0)
+                {
+                    System.out.println("Die Zeit läuft nicht rückwärts!");   
+                }    
+            }
+            else
+            {
+                System.out.println("Gib eine Laufzeit ein!");
+            }
+        }
+        catch(NumberFormatException e)
+        {
+            float b = Float.parseFloat(args[0]); 
+            int n = 1000;
+            int[] arr2 = makeArray(n);
+            while(b > runtime(arr2))
+            {
+                n = 2*n;
+                arr2 = makeArray(n);
+                System.out.println("Größe des Feldes: " +n +" Laufzeit: " +runtime(arr2));
+            } 
+            int tmpN = n/2;
+            while(n >= tmpN)
+            {
+                int middle = (n+tmpN)/2;
+                int[] arr3 = makeArray(middle); 
+                float rtMid = runtime(arr3);
+                if(rtMid+0.1 <= b || rtMid-0.1 >= b)
+                {
+                    System.out.println("Größe des Feldes: " +arr3.length + " Laufzeit: " +rtMid);
+                }
+                if(rtMid < b)
+                {
+                    tmpN = middle+1;
+                }
+                if(rtMid > b)
+                {
+                    n = middle-1;
+                }
+            }
+        }
+    }
 }
+
+   
